f5d1481e2409b9b108d78f092cd81eff634ae4b8
loforsteph <3089954938@qq.com>
2023-07-12 10:20:39 +0800

    Add Student model and Teacher model

--- codes/menu/models.py
+++ codes/menu/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    name=models.CharField(max_length=20,default="noone")
+    salary=models.IntegerField(default=0)
+    selfDesc=models.CharField(max_length=100)
+    subject=models.CharField(max_length=20)
+    w_sex=models.CharField(max_length=20)
+    age=models.CharField(max_length=20)
+    w_edu=models.CharField(max_length=20)
+    w_time=models.IntegerField()
+    mail=models.CharField(max_length=50)
+    
+class Teacher(models.Model):
+    name = models.CharField(max_length=20,default="Anonymous")
+    salary = models.PositiveIntegerField(default=0)
+    selfDesc = models.CharField(max_length=100,default='')
+    subjects = models.CharField(max_length=20)
+    sex = models.CharField(max_length=4, default="Null")
+    edu = models.CharField(max_length=20,default='Null')
+    time = models.PositiveIntegerField()
+    mail = models.CharField(max_length=30)
--- codes/menu/views.py
+++ codes/menu/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import loader
+from .models import Teacher
+from .models import Student
 # Create your views here.
+def form(request):
+    template=loader.get_template('form.html')
+    return HttpResponse(template.render())
